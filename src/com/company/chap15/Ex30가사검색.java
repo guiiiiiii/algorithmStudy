0fc67a0084014d3ce07f2747e8f57b1b342d2502
guri <nocandidates@naver.com>
2022-03-28 23:35:48 +0900

    0328 가사검색 완성

--- src/com/company/chap15/Ex30가사검색.java
+++ src/com/company/chap15/Ex30가사검색.java
@@ -20,26 +20,34 @@ public class Ex30가사검색 {
         int[] answer = new int[queries.length];
         int count = 0;
 
-        HashMap<Integer, ArrayList<String>> wordsMap = new HashMap<>();
-        HashMap<Integer, ArrayList<String>> wordsReverseMap = new HashMap<>();
+        ArrayList<ArrayList<String>> wordsList = new ArrayList<ArrayList<String>>();
+        ArrayList<ArrayList<String>> reverseWordsList = new ArrayList<ArrayList<String>>();
 
-        // Step0. words를 글자수대로 분류하여 hashmap에 넣어준다
+        // Step1. words를 글자수대로 분류하여 관리하기위해 가능한 수만큼 arraylist를 생성해준다
+        for(int i=0; i<10001; i++){
+            wordsList.add(new ArrayList<>());
+            reverseWordsList.add(new ArrayList<>());
+        }
+
+        // Step1. words를 글자수대로 분류하여  넣어준다
         //        이 때 ?가 앞에 있는 query면 뒤에서부터 비교하기위해 (자바 compareTo 함수는 같은 자리수부터 비교하기 때문)
         //        기본 주어진 word와 반대로 뒤집힌 reverseWord를 저장하는 map 두개를 생성해서 사용한다
         for(String word : words){
             int size = word.length();
-            ArrayList<String> tempList = wordsMap.getOrDefault(size, new ArrayList<String>());
-            ArrayList<String> tempReverseList = wordsReverseMap.getOrDefault(size, new ArrayList<String>());
-            tempList.add(word);
+            wordsList.get(size).add(word);
 
             String reversedStr = (new StringBuffer(word)).reverse().toString();
-            tempReverseList.add(reversedStr);
+            reverseWordsList.get(size).add(reversedStr);
 
-            wordsMap.put(size, tempList);
-            wordsReverseMap.put(size, tempReverseList);
         }
 
-        // Step1. queries를 순회하며 첫 인자부터 check한다
+        // Step2. 단어를 사전순으로 정렬
+        for(int i=0; i<10001; i++){
+            Collections.sort(wordsList.get(i));
+            Collections.sort(reverseWordsList.get(i));
+        }
+
+        // Step3. queries를 순회하며 첫 인자부터 check한다
         for(int arrIndex = 0; arrIndex < queries.length; arrIndex++){
             String query = queries[arrIndex];
             count = 0;
@@ -47,30 +55,23 @@ public class Ex30가사검색 {
             int upperIndex;
             int lowerIndex;
 
-            // Step2. 앞에 ?가 있는지 뒤에?가 있는지 체크한 뒤, 가작 작은(가장 큰) 인덱스를 구한다
+            // Step4. 앞에 ?가 있는지 뒤에?가 있는지 체크한 뒤, 가작 작은(가장 큰) 인덱스를 구한다
             if(query.startsWith("?")){
                 // ?로 시작한다면 문자를 반대로 뒤집은 다음 upperindex와 lowerindex를 찾는다
-                ArrayList<String> wordsArr = wordsReverseMap.get(query.length());
+                ArrayList<String> wordsArr = reverseWordsList.get(query.length());
 
-                if(wordsArr != null){
-                    Collections.sort(wordsArr);
-                    lowerIndex = findLowerIndex(wordsArr, (new StringBuffer(query)).reverse().toString().replaceAll("\\?", "a"), 0, wordsArr.size()-1);
-                    upperIndex = findUpperIndex(wordsArr, (new StringBuffer(query)).reverse().toString().replaceAll("\\?", "z"), 0, wordsArr.size()-1);
+                lowerIndex = findLowerIndex(wordsArr, (new StringBuffer(query)).reverse().toString().replaceAll("\\?", "a"), 0, wordsArr.size()-1);
+                upperIndex = findUpperIndex(wordsArr, (new StringBuffer(query)).reverse().toString().replaceAll("\\?", "z"), 0, wordsArr.size()-1);
 
-                    count = upperIndex - lowerIndex +1;
-                }
+                count = upperIndex - lowerIndex +1;
 
             } else{
-                ArrayList<String> wordsArr = wordsMap.get(query.length());
+                ArrayList<String> wordsArr = wordsList.get(query.length());
 
-                if(wordsArr != null){
-                    Collections.sort(wordsArr);
-                    lowerIndex = findLowerIndex(wordsArr, query.replaceAll("\\?", "a"), 0, wordsArr.size()-1);
-                    upperIndex = findUpperIndex(wordsArr, query.replaceAll("\\?", "z"), 0, wordsArr.size()-1);
+                lowerIndex = findLowerIndex(wordsArr, query.replaceAll("\\?", "a"), 0, wordsArr.size()-1);
+                upperIndex = findUpperIndex(wordsArr, query.replaceAll("\\?", "z"), 0, wordsArr.size()-1);
 
-                    count = upperIndex - lowerIndex +1;
-
-                }
+                count = upperIndex - lowerIndex +1;
 
             }
 
